c83744cbd053a5a4e0bd41e516aac11044c9ddec
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-04-05 22:45:19 +0300

    Create constants.dart
    to store our constants

---
+++ from_the_greats/lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPictureBackgroundColor = Color(0xFF1D1E33);
+
+const headerMultiPageTextStyle = TextStyle(
+  fontFamily: 'Satisfy',
+  fontSize: 20.0,
+);
