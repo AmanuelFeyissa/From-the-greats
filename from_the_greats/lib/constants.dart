aa5b5148ca2ebbcdce9d291ebbb1c84a40b5c0d1
Amanuel Feyissa <amanuelfeyissa45@gmail.com>
2022-04-06 10:07:37 +0300

    Update constants.dart
    added fontSize

--- from_the_greats/lib/constants.dart
+++ from_the_greats/lib/constants.dart
@@ -4,7 +4,8 @@ const kPictureBackgroundColor = Color(0xFF1D1E33);
 
 const headerMultiPageTextStyle = TextStyle(
   fontFamily: 'Satisfy',
-  fontSize: 20.0,
+  fontSize: 50.0,
 );
 
-const descriptionMultiPageTextStyle = TextStyle(fontFamily: 'Serif Pro');
+const descriptionMultiPageTextStyle =
+    TextStyle(fontFamily: 'Serif Pro', fontSize: 25.0);
